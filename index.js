bc9e3639d721d35b440e6034598d555d783b034b
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-04-26 02:47:03 +0200

    ch6

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ app.listen(4000, ()=>{
     console.log('App listening on port 4000')
 })
 
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get('/',async (req,res)=>{    
+    const blogposts = await BlogPost.find({})         
+    res.render('index',{
+        blogposts
+    });
 })
 
 app.get('/about',(req,res)=>{
@@ -33,19 +36,25 @@ app.get('/contact',(req,res)=>{
     res.render('contact');
 })
 
-app.get('/post',(req,res)=>{
+/* app.get('/post',(req,res)=>{
     //res.sendFile(path.resolve(__dirname, 'pages/post.html'))
     res.render('post');
+})
+ */
+app.get('/post/:id',async (req,res)=>{        
+    const blogpost = await BlogPost.findById(req.params.id)
+    res.render('post',{
+        blogpost
+    });    
 })
 
 app.get('/posts/new',(req,res)=>{
     res.render('create')
 })
 
-app.post('/posts/store',(req,res)=>{
-    //model creates a new doc with browser data
-    BlogPost.create(req.body,(error,blogspot) =>{
+app.post('/posts/store', async (req,res)=>{
+    await BlogPost.create(req.body)
      res.redirect('/')    
-    })  
+    
 })
 
