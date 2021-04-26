6c91fa154aa85df1aa7a03687df65f3622885758
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2021-04-26 15:49:04 +0200

    ch9acabat

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
-const path = require('path')
+/* const path = require('path') */
 
 const app = new express()
 const ejs = require('ejs')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const BlogPost = require('./models/BlogPost.js')
+/* const BlogPost = require('./models/BlogPost.js') */
+const fileUpload = require('express-fileupload')
+
+const newPostController = require('./controllers/newPost')
+const homeController = require('./controllers/home')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const newUserController = require('./controllers/newUser')
+
+const validateMiddleWare = require("./middleware/validateMiddleware");
+
+
+
+app.use(fileUpload()) 
 
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
 app.use(express.json())
@@ -19,42 +32,20 @@ app.listen(4000, ()=>{
     console.log('App listening on port 4000')
 })
 
-app.get('/',async (req,res)=>{    
-    const blogposts = await BlogPost.find({})         
-    res.render('index',{
-        blogposts
-    });
-})
-
-app.get('/about',(req,res)=>{
+/* app.get('/about',(req,res)=>{
    // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
    res.render('about');
-})
+}) */
 
-app.get('/contact',(req,res)=>{
+/* app.get('/contact',(req,res)=>{
     //res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
     res.render('contact');
-})
+}) */
 
-/* app.get('/post',(req,res)=>{
-    //res.sendFile(path.resolve(__dirname, 'pages/post.html'))
-    res.render('post');
-})
- */
-app.get('/post/:id',async (req,res)=>{        
-    const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post',{
-        blogpost
-    });    
-})
+app.use('/posts/store',validateMiddleWare) 
 
-app.get('/posts/new',(req,res)=>{
-    res.render('create')
-})
-
-app.post('/posts/store', async (req,res)=>{
-    await BlogPost.create(req.body)
-     res.redirect('/')    
-    
-})
-
+app.get('/posts/new',newPostController)
+app.get('/',homeController)
+app.get('/post/:id',getPostController)        
+app.post('/posts/store', storePostController)
+app.get('/auth/register', newUserController)
